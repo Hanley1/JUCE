412e1983ba8095a562461aa441bb83231dd8e91e
Joe Hanley <joe@syntorial.com>
2023-05-17 17:30:11 -0400

    Added Dropbox SDK update back in from 1.6.605

--- modules/juce_gui_basics/native/juce_ios_Windowing.mm
+++ modules/juce_gui_basics/native/juce_ios_Windowing.mm
@@ -247,6 +247,27 @@ namespace juce
     }
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    
+#ifndef IS_PRIMER
+    
+    DBOAuthCompletion completion = ^(DBOAuthResult *authResult) {
+       if (authResult != nil) {
+         if ([authResult isSuccess]) {
+           NSLog(@"\n\nSuccess! User is logged into Dropbox.\n\n");
+         } else if ([authResult isCancel]) {
+           NSLog(@"\n\nAuthorization flow was manually canceled by user!\n\n");
+         } else if ([authResult isError]) {
+           NSLog(@"\n\nError: %@\n\n", authResult);
+         }
+       }
+     };
+     BOOL canHandle = [DBClientsManager handleRedirectURL:url completion:completion];
+     return canHandle;
+#endif
+}
+
 - (void) applicationWillEnterForeground: (UIApplication*) application
 {
     ignoreUnused (application);
